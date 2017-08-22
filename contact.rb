33b2a0fedf7909ebe27687cfddf1a2a8dccb9013
Anthematics <jasonharder2@gmail.com>
2017-08-21 20:28:54 -0400

    v 1.2  Having issues with search by attribute as it double prints menu. this is as good as it gets tonight where i can maintain my sanity.

--- contact.rb
+++ contact.rb
@@ -1,103 +1,18 @@
-require 'pry'
-class Contact
-	@@allcontacts = [{first_name: @first_name, last_name: @last_name, email: @email, note: @note} ]
-	@@accountnumbers = 1000
+require 'active_record'
+require 'mini_record'
 
-	# This method should initialize the contact's attributes
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm.sqlite3')
 
-	def initialize (first_name,last_name,email,note)
-		@accountid = @@accountnumbers+=1
-		@first_name = first_name
-		@last_name = last_name
-		@email = email
-		@note = note
-	end
+class Contact < ActiveRecord::Base
+	field :first_name, as: :string
+	field :last_name,  as: :string
+	field :email,      as: :string
+	field :note,       as: :text
 
-
-
-	#create method works (tested)
-	def self.create(first_name,last_name,email,note)
-		acreate=Contact.new(first_name,last_name,email,note)
-		@@allcontacts << acreate #shovel into all contacts array
-		acreate
-	end
-
-	#readers - so values can be accessed.
-	def first_name
-		@first_name
-	end
-
-	def last_name
-		@last_name
-	end
-
-	def email
-		@email
-	end
-
-	def note
-		@note
-	end
-	# Writer methods (to allow changes to be made )
-
-	# This method should return all of the existing contacts (doesnt seem to be culprit )
-	def self.all
-		@@allcontacts
-			return @@allcontacts
-	end
-
-	# This method should accept an id as an argumenttax
-# 	# and return the contact who has that id - this method is safe -commented out not causing 101 syn
-	def self.find
-		user=gets.chomp
-			accounts.each do|user|
-				if user=@@accountnumbers
-					return user
-				end
-			end
-	end
-# 	end
-#
-# 	# This method should allow you to specify
-# 	# (writer)
-# 	# 1. which of the contact's attributes you want to update
-# 	# 2. the new value for that attribute
-# 	# and then make the appropriate change to the contact
-# 	def update
-#
-# 	end
-#
-# 	# findby #nosyntax errors - needs testing
-	def self.find_by
-		user=gets.chomp
-			accounts.each do |user|
-				if user == @first_name | @last_name | @email #shouldinteratethroughthearrayandreturnvaluesthat
-					return user
-				end
-			end
-	end
-#
-# #delete all - no errors plez test.
-	def self.delete_all
-		@@allcontacts.clear
-	end
-#
-#no errors plez test.
 	def full_name
-		full_name= @first_name+ @last_name
-	end
-#
-# 	# This method should delete the contact (tested no syntax)
-# 	# HINT: Check the Array class docs for built-in methods that might be useful here
-	def delete
-		 user=gets.chomp
-			accounts.each do|user|
-				@@allcontacts.slice(user)  == @first_name| @last_name| @email
-			end
+		"#{ first_name } #{ last_name }"
 	end
 
 end
 
-jason=Contact.create("jason","pimp","jason@pimp.pimp","thisguy")
-matthew=Contact.create("matthew","alsokindofcool","wer@g.com","brother")
-self.inspect
+Contact.auto_upgrade!
--- crm.rb
+++ crm.rb
@@ -1,73 +1,184 @@
-require_relative 'contact'
+require_relative 'contact.rb'
 
-class CRM
+class Crm #DON'T I DARE EVER IN MY LIFE INHERIT AND REQIRE FROM THE SAME SOURCE WITHOUT CALLING SUPER (in this case we dont want to because
+	# we will create a contact and a new instance of crm )
 
 	def initialize
 
 	end
 
-	# def name
-	# 	= @first_name @last_name
-	# end
-
-
 	def main_menu
 		while true
 			print_main_menu
-			user_selected = gets.to_i
+			user_selected = gets.chomp.to_i
 			call_option(user_selected)
 		end
 	end
 
-
 	def print_main_menu
-		puts '[1] Add a new contact'
-		puts '[2] Modify an existing contact'
-		puts '[3] Delete a contact'
-		puts '[4] Display all the contacts'
-		puts '[5] Search by attribute'
-		puts '[6] Exit'
-		puts 'Enter a number: '
+
+	 puts '[1] Add a new contact'
+	 puts '[2] Modify an existing contact'
+	 puts '[3] Delete a contact'
+	 puts '[4] Display all the contacts'
+	 puts '[5] Search by attribute'
+	 puts '[6] Exit'
+	 puts 'Enter a number: '
 	end
 
-	def call_option
-		case user_selected
-			when 1 then add_new_contact
-			when 2 then modify_existing_contact
-			when 3 then delete_contact
-			when 4 then display_all_contacts
-			when 5 then search_by_attribute
-			when 6 then exit.
-			end
+
+	def print_modify_menu
+		puts '[1] change the first name'
+		puts '[2] change the last name'
+		puts '[3] change the email'
+		puts '[4] change your notes'
+		puts 'Enter a number:'
+	end
+
+	def search_menu(user_selected3)
+		case user_selected3
+		when 1 then find_byfirst_name
+		when 2 then find_bylast_name
+		when 3 then find_byemail
 		end
+	end
+
+	def print_search_options
+		puts '[1] find by first name'
+		puts '[2] find by last name '
+		puts '[3] find by email'
+		# user_selected0 = gets.chomp.to_i
+		search_menu(gets.chomp.to_i)
+	end
+
+	def modify_option(user_selected2)
+		case user_selected2
+		when 1 then changefirst_name
+		when 2 then changelast_name
+		when 3 then changeemail
+		when 4 then changenote
+		end
+	end
+end
+
+	def find_byfirst_name
+
+	end
+
+	def find_bylast_name
+
+	end
+
+	def find_byemail
+
+	end
+
+	def changefirst_name
+		puts "enter the first name"
+		firstname = gets.chomp
+		contact = Contact.where(first_name:  firstname)
+		puts "enter the new first name"
+		new_first_name  = gets.chomp
+		contact.update_all(first_name: new_first_name)
+	end
+
+	def changelast_name
+		puts "enter the last name"
+		lastname=gets.chomp
+		contact = Contact.where(last_name:lastname)
+		puts "enter the new last name"
+		new_last_name = gets.chomp
+		contact.update_all(last_name:new_last_name)
+	end
+
+	def changeemail
+		puts "enter the email address you'd like to change"
+		emailaddy=gets.chomp
+		contact = Contact.where(email:emailaddy)
+		puts "enter the new email address"
+		new_email = gets.chomp
+		contact.update_all(email:new_email)
+	end
+
+	def changenote
+			puts "enter the email address of the contact you'd like to change the note for"
+			emailaddy2=gets.chomp
+			contact = Contact.where(email:emailaddy2)
+			puts "enter the new note"
+			new_note  = gets.chomp
+			contact.update_all(note: new_note)
+	end
+
+	def delete_contact
+		puts "enter the email of the contact you would like to delete"
+		emailaddy3 = gets.chomp.to_s #to string??
+		emaildel = Contact.where(email:emailaddy3)
+		Contact.delete(emailaddy3)
+	end
+
+	def display_all_contacts
+		p Contact.all
+	end
+
+	def search_by_attribute
+#iterate through all contacts and return the one by its key or value?
+		puts "search contact by email, first or last name"
+		Contact.find_by
+	end
+
+
+	def modify_menu
+			print_modify_menu
+			user_selected = gets.to_i
+			modify_option(user_selected)
+	end
+
+	def search_options
+		print_search_options
+
 	end
 
 	def add_new_contact
 
+		print 'Enter the First Name: '
+		first_name = gets.chomp.to_s #must be saved as a string
+
+		print 'Enter the Last Name: '
+		last_name = gets.chomp.to_s
+
+		print 'Enter and Email Address: '
+		email = gets.chomp.to_s
+
+		print 'Enter a Note: '
+		note = gets.chomp.to_s
+# binding.pry
+
+	contact = Contact.create(
+		first_name: first_name,
+		last_name:  last_name,
+		email:      email,
+		note:       note
+	)
 	end
 
-	def modify_existing_contact
 
-	end
-
-	def delete_contact
-
-	end
-
-	def display_all_contacts
-
-	end
-
-	def search_by_attribute
-
-	end
+		def call_option(user_selected)
+			case user_selected
+			when 1 then add_new_contact
+			when 2 then modify_menu
+			when 3 then delete_contact
+			when 4 then display_all_contacts
+			when 5 then search_options
+			when 6 then exit.
+			end
+		end
 
 
 end
 
-a_crm_app = CRM.new
-a_crm_app.main_menu
-a_crm_app.print_main_menu
+at_exit do
+	ActiveRecord::Base.connection.close
+end
 
-jason=Contact.create("jason","pimp","jason@pimp.pimp","thisguy")
-jason.inspect
+a = Crm.new
+a.main_menu
